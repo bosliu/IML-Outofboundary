eeb7b913027b21a3e2d335affff32bb68067765f
juhe9842 <juhe9842@colorado.edu>
2022-03-11 18:30:36 +0800

    Add Newton's method with momentum; Results converged as desired

--- task0/main.py
+++ task0/main.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 
@@ -11,6 +12,14 @@ train.head()
 train_mat: np.ndarray = train.to_numpy()
 train_y: np.ndarray = train_mat[:, 1]
 train_x: np.ndarray = train_mat[:, 2:]
+
+range_x: np.ndarray = np.max(train_x, axis=0) - np.min(train_x, axis=0)
+train_x_avg: np.ndarray = np.divide(train_x - np.min(train_x, axis=0), range_x)
+x_min = np.min(train_x, axis=0)
+
+range_y: float = np.max(train_y) - np.min(train_y)
+train_y_avg: np.ndarray = np.divide(train_y - np.min(train_y), range_y)
+y_min = np.min(train_y)
 
 
 # %%
@@ -44,16 +53,16 @@ class ObjFuncSDG(ObjFunc):
 
 # %%
 def gradient_descent(obj_func: ObjFunc, w_init: np.ndarray, lr: float, tol: float, max_steps: int) -> Tuple[
-    np.ndarray, List, List]:
+        np.ndarray, List, List]:
     w_curr = w_init
     w_hist: List[np.ndarray] = [w_init]
     obj_hist: List[Union[float, np.ndarray]] = [obj_func(w_curr)]
     error = np.zeros_like(w_curr)
     for step in range(max_steps):
         direction: np.ndarray = obj_func.grad(w_curr)
-        w_next = error*0.99+w_curr - lr * direction
+        w_next = lr * 0.99 * error + w_curr - lr * direction
         error = w_next - w_curr
-        print(obj_func(w_curr))
+        # print(obj_func(w_curr))
         if np.allclose(w_next, w_curr, tol):
             break
         w_curr = w_next
@@ -65,44 +74,75 @@ def gradient_descent(obj_func: ObjFunc, w_init: np.ndarray, lr: float, tol: floa
 # %%
 def sgd(obj_func: ObjFuncSDG, w_init: np.ndarray, lr: float, tol: float, n_epoch: int, batch_size: int) -> np.ndarray:
     w_curr = w_init
+    rand_idx: np.ndarray = np.array(
+        [x for x in range(obj_func.n)], dtype=int)
+    np.random.shuffle(rand_idx)
+    error = np.zeros_like(w_curr)
     for epoch in range(n_epoch):
-        rand_idx: np.ndarray = np.array([x for x in range(obj_func.n)], dtype=int)
-        np.random.shuffle(rand_idx)
-        error = np.zeros_like(w_curr)
+        # lr = max(0.9*lr, 1e-16)
         for i in range(0, obj_func.n, batch_size):
             batch_idx: np.ndarray = rand_idx[i: i + batch_size]
             direction: np.ndarray = obj_func.grad(w_curr, batch_idx)
-            #direction = direction / np.linalg.norm(direction)
-            w_next = error * 0.9 - lr * direction + w_curr
+            # direction = direction / np.linalg.norm(direction)
+            w_next = w_curr - lr * direction + 0.8 * error
             error = w_next - w_curr
-            print(obj_func(w_curr))
-            if np.allclose(w_next, w_curr, tol):
+            # print(obj_func(w_curr))
+            if np.linalg.norm(w_next-w_curr) <= tol:
+                print("Converged")
+                print(lr)
                 return w_next
             w_curr = w_next
+        print(np.linalg.norm(direction))
+    print(lr)
     return w_next
 
+
+def Newton(obj_func: ObjFuncSDG, w_init: np.ndarray, lr: float, tol: float, n_epoch: int, batch_size: int) -> np.ndarray:
+    w_curr = w_init
+    batch_idx: np.ndarray = np.array(
+        [x for x in range(obj_func.n)], dtype=int)
+    error = np.zeros_like(w_curr)
+    for epoch in range(n_epoch):
+        direction: np.ndarray = obj_func.grad(w_curr, batch_idx)
+        w_next = w_curr - \
+            np.linalg.inv(obj_func.phi.T  @
+                          obj_func.phi) @ direction + 0.8 * error
+        error = w_next - w_curr
+        if np.linalg.norm(w_next-w_curr) <= tol:
+            print("Converged")
+            return w_next
+        w_curr = w_next
+        print("direction: ", np.linalg.norm(direction))
+    return w_next
 
 # %%
 # hyperparams
 
-learning_rate = 0.01
-tol = 1e-6
-n_steps = 100
+
+learning_rate = 0.001
+tol = 1e-12
+# n_steps = 100
 
 batch_size = 100
-num_of_epochs = 100
+num_of_epochs = 19000
 # %%
-phi = train_x.copy().astype(np.float64)
+phi = train_x_avg.copy().astype(np.float64)
 phi = np.insert(phi, 0, 1, axis=1)
 # %%
 w_init = np.random.random(11)
+fitting = ObjFuncSDG(y=train_y_avg, phi=phi)
+w_res = Newton(fitting, w_init, lr=learning_rate, tol=tol,
+               n_epoch=num_of_epochs, batch_size=batch_size)
 
-fitting = ObjFunc(y=train_y, phi=phi)
-#fitting = ObjFuncSDG(y=train_y, phi=phi)
+# use scikit_learn to compare the results
+reg = LinearRegression().fit(phi, train_y_avg)
+range_x = np.insert(range_x, 0, 1)
+# parameters recover
+w = np.multiply(range_y, reg.coef_) / range_x
+print("sklearn result: ", w)
 
-w_res, _, _ = gradient_descent(fitting, w_init, lr=learning_rate, tol=tol, max_steps=n_steps)
-#w_res = sgd(fitting, w_init, lr=learning_rate, tol=tol, n_epoch=num_of_epochs, batch_size=batch_size)
-
-#C = np.linalg.eig(phi.T@phi)
-#print("C", C[0])
-
+# Newton's method
+# parameters recover
+w_sgd = range_y * w_res / range_x
+w_sgd[0] += - (w_sgd[1:] @ x_min) + y_min
+print("Newton's result: ", w_sgd)
