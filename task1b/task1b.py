aa78e452ce42f8f96f8c330e42e9d9d1ce78a050
HanW <wanghan19990302@gmail.com>
2022-03-22 23:52:37 +0100

    Update task1b.py
    Simplified code by optimization

--- task1b/task1b.py
+++ task1b/task1b.py
@@ -10,19 +10,7 @@ if __name__ == '__main__':
     y = train_mat[:, 1]
     X = train_mat[:, 2:]
 
-    x1, x2, x3, x4, x5 = X[:, 0:1], X[:, 1:2], X[:, 2:3], X[:, 3:4], X[:, 4:5]
-    xs = (x1, x2, x3, x4, x5)
-
-    quad = lambda xseq: tuple(np.square(x) for x in xseq)
-    expo = lambda xseq: tuple(np.exp(x) for x in xseq)
-    cosine = lambda xseq: tuple(np.cos(x) for x in xseq)
-
-    phi1 = np.concatenate(xs, axis=1)
-    phi2 = np.concatenate(quad(xs), axis=1)
-    phi3 = np.concatenate(expo(xs), axis=1)
-    phi4 = np.concatenate(cosine(xs), axis=1)
-    phi5 = np.ones_like(y).reshape(-1, 1)
-    phi = np.concatenate((phi1, phi2, phi3, phi4, phi5), axis=1)
+    phi = np.concatenate((X, np.square(X), np.exp(X), np.cos(X), np.ones_like(y).reshape(-1, 1)), axis=1)
 
     model = LinearRegression()
     model.fit(phi, y)
