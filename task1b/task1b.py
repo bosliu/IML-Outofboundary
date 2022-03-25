ad43fdb222268d1231e147df84d3a0abed1c922c
HanW <wanghan19990302@gmail.com>
2022-03-25 20:12:42 +0100

    Added the closed-form solution for task 1b
    R.T. The closed-form solution is implemented. The result is the same compared with the sklearn one.

--- task1b/task1b.py
+++ task1b/task1b.py
@@ -15,3 +15,10 @@ if __name__ == '__main__':
     model = LinearRegression(fit_intercept=False)
     model.fit(phi, y)
     print(model.coef_)
+    
+    # closed form solution below
+    w_cfs = np.linalg.pinv(phi.T @ phi) @ phi.T @ y
+    print(w_cfs)
+
+    df = pd.DataFrame(w_cfs)
+    df.to_csv('result_cfs.csv', index=False, header=False)
