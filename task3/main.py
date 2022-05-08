69922609a3724d47abda27ccf728ad596a1e5b53
HanW <wanghan19990302@gmail.com>
2022-05-08 11:02:34 +0200

    I hope this commit works

--- task3/main.py
+++ task3/main.py
@@ -37,7 +37,7 @@ from typing import Dict, Generator, List, Optional
             |---- processed
                 |
                 |---- *.jpg (Images, Processed)
-   
+
 """
 # hyperparams
 IMAGE_DIM = (460, 310)
@@ -57,6 +57,7 @@ MODEL_PATH = 'trained_models/'
 MODEL_FILE_NAME = 'model_epoch_10.pth'
 TEST = False
 
+
 class Preprocess:
     def __init__(self, img_path, lbl_path, use_preprocessed_img=True) -> None:
         self.lbl_path = lbl_path
@@ -65,7 +66,8 @@ class Preprocess:
             processed_img_path = os.path.join(img_path, 'processed')
             self.img_path = processed_img_path
             try:
-                assert len(os.listdir(processed_img_path)) == len([f for f in os.listdir(img_path) if f.endswith('.jpg')])
+                assert len(os.listdir(processed_img_path)) == len(
+                    [f for f in os.listdir(img_path) if f.endswith('.jpg')])
                 print("Processed images found at:" + self.img_path)
             except:
                 self.to_perform = True
@@ -86,11 +88,11 @@ class Preprocess:
         for f in os.listdir(self.img_path):
             if f.endswith(".jpg"):
                 yield f
-    
+
     def check_img_num(self):
         self.img_num = len(list(set(self.jpg_gen())))
         print(f"Total # of img found: {self.img_num}.")
-    
+
     def load_img(self, out=False) -> Optional[List[np.ndarray]]:
         images = []
         state = " Loading images " if not self.to_perform else " Loading and Preprocessing images "
@@ -118,7 +120,7 @@ class Preprocess:
         if TRAIN:
             with open(self.lbl_path + '/train_triplets.txt', 'r') as f:
                 for l in f:
-                    triplet = tuple(map(int, l.split( )))
+                    triplet = tuple(map(int, l.split()))
                     self.train_triplets_img.extend(list(triplet))
                     train_triplets.append(triplet)
             self.train_triplets = train_triplets
@@ -128,20 +130,21 @@ class Preprocess:
         if TEST:
             with open(self.lbl_path + '/test_triplets.txt', 'r') as f:
                 for l in f:
-                    triplet = tuple(map(int, l.split( )))
+                    triplet = tuple(map(int, l.split()))
                     self.test_triplets_img.extend(list(triplet))
                     test_triplets.append(triplet)
             self.test_triplets = test_triplets
             self.test_triplets_img = list(set(self.test_triplets_img))
             print(f"Test labels read with total length of {len(self.test_triplets)}.")
-    
+
     def split_validation(self):
         if VALIDATION:
-            train_len = int(len(self.train_triplets) * (1- VALIDATION_PROP))
-            self.train_triplets, self.validation_triplets = self.train_triplets[:train_len], self.train_triplets[train_len:]
+            train_len = int(len(self.train_triplets) * (1 - VALIDATION_PROP))
+            self.train_triplets, self.validation_triplets = self.train_triplets[:train_len], self.train_triplets[
+                                                                                             train_len:]
         else:
             pass
-    
+
     def img_process(self, i, img):
         # image process here...
         assert img is not None
@@ -164,19 +167,20 @@ class FoodDataSet(Dataset):
         self.tf = transform if transform != None else T.Compose(
             T.ToTensor(),
             T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            )
-    
+        )
+
     def __getitem__(self, index):
         a, b, c = self.triplets[index]
         ims = [Image.fromarray(cv.cvtColor(images[i], cv.COLOR_BGR2RGB)) for i in (a, b, c)]
         return tuple([self.tf(im) for im in ims])
-    
+
     def __len__(self):
         return len(self.triplets)
 
 
 class NeuralNet(nn.Module):
     EMBEDDING_DIM = 1024
+
     # def __init__(self, inp: int = 2208, hidden: int = 384, hidden_2: int = 128, d: float = 0.2):
     #     super().__init__()
     #     self.fc = nn.Sequential(
@@ -191,7 +195,7 @@ class NeuralNet(nn.Module):
     #         # nn.Dropout(d),
     #         nn.Linear(hidden_2, self.EMBEDDING_DIM)
     #     )
-    
+
     # def __init__(self, inp: int = 2208, hidden: int = 512, d: float = 0.2):
     #     super().__init__()
     #     self.fc = nn.Sequential(
@@ -202,8 +206,8 @@ class NeuralNet(nn.Module):
     #         nn.Dropout(d),
     #         nn.Linear(hidden, self.EMBEDDING_DIM)
     #     )
-    
-    def __init__(self, inp: int = 512,):
+
+    def __init__(self, inp: int = 512, ):
         super().__init__()
         self.fc = nn.Sequential(
             nn.Flatten(),
@@ -214,7 +218,7 @@ class NeuralNet(nn.Module):
     def forward(self, x):
         x = self.fc(x)
         return nn.functional.normalize(x)
-    
+
     # def init_weights(self, m):
     #     if isinstance(m, nn.Conv2d):
     #         torch.nn.init.kaiming_normal_(m.weight)
@@ -222,13 +226,13 @@ class NeuralNet(nn.Module):
 
 def closest_image(a, b, c):
     """
-    Returns: 
-        int: 1 if a is more similar to the positive image b. 
+    Returns:
+        int: 1 if a is more similar to the positive image b.
                 0 if a is more similar to the negative image c.
-                
+
     Note:
-        Works for batches. 
-        Each input tensor should have this shape: `N x W x H x C` 
+        Works for batches.
+        Each input tensor should have this shape: `N x W x H x C`
         where `N` is the number of samples.
     """
     if CRITERION == 'dist':
@@ -264,7 +268,7 @@ def initialize_model(model_name, freeze_pretrained_features, use_pretrained=True
         model_ft = torchvision.models.densenet161(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, freeze_pretrained_features)
         num_ftrs = model_ft.classifier.in_features
-        model_ft.classifier = NeuralNet(inp=num_ftrs) 
+        model_ft.classifier = NeuralNet(inp=num_ftrs)
         input_size = 224
     elif model_name == "resnet":
         """ Resnet152
@@ -273,7 +277,7 @@ def initialize_model(model_name, freeze_pretrained_features, use_pretrained=True
         model_ft = torchvision.models.resnet18(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, freeze_pretrained_features)
         num_ftrs = model_ft.fc.in_features
-        model_ft.fc = NeuralNet(inp=num_ftrs) 
+        model_ft.fc = NeuralNet(inp=num_ftrs)
         input_size = 224
     elif model_name == "inception":
         """ Inception v3
@@ -300,38 +304,38 @@ class AverageMeter:
     avg: float = 0
     sum: float = 0
     count: int = 0
-    
+
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-    
+
     def __str__(self):
         return f'{self.name}: {self.val:.3f}, avg: {self.avg:.3f}'
 
 
-def train(model, optimizer, train_loader, criterion, batch_size, epochs, scheduler=None, 
+def train(model, optimizer, train_loader, criterion, batch_size, epochs, scheduler=None,
           model_name='model', val_loader=None, starting_epoch=0):
     print("Batch per epoch:" + str(len(train_loader)))
     starting_epoch = int(starting_epoch)
     print(f"Starting at epoch {starting_epoch + 1}.")
     pbar = tqdm(desc='Training', total=epochs * len(train_loader), leave=False)
-    
+
     if not os.path.exists(model_path):
         print(".... Making dir at " + str(model_path))
         os.mkdir(model_path)
-    
+
     curr_lr = 0
     if val_loader:
         val_acc = 0
         best_epoch = -1
         val_loss = 100
-    
+
     assert starting_epoch < epochs
     for epoch in range(epochs):
         epoch += starting_epoch
-        print(f"\n>> Epoch {epoch+1} ")
+        print(f"\n>> Epoch {epoch + 1} ")
         if STEP_LR:
             curr_lr = scheduler.get_last_lr()[-1]
             print(f"Current learning rate: {curr_lr:.3e}.")
@@ -346,21 +350,21 @@ def train(model, optimizer, train_loader, criterion, batch_size, epochs, schedul
             imc = imc.to(device)
             pbar.set_description_str(f'Epoch {epoch + 1}, batch {batch_idx + 1}')
             optimizer.zero_grad()
-            
+
             out = tuple(map(model, [ima, imb, imc]))
             loss = criterion(*out)
             losses.update(loss.item(), ima.shape[0])
             loss.backward()
             optimizer.step()
-            
+
             pred_labels = closest_image(*out).detach().cpu().numpy()
             acc = accuracy(pred_labels, np.ones_like(pred_labels))
-            acc_epoch.update(acc/ima.shape[0], ima.shape[0])
+            acc_epoch.update(acc / ima.shape[0], ima.shape[0])
             pbar.set_postfix_str("{}, accuracy {}/{}".format(losses, acc, ima.shape[0]))
             pbar.update()
-        print(f"\nEpoch {epoch+1} ended with average loss {losses.avg} and overall accuracy {acc_epoch.avg:.3f}.")
+        print(f"\nEpoch {epoch + 1} ended with average loss {losses.avg} and overall accuracy {acc_epoch.avg:.3f}.")
 
-            # do validation here to follow the training process
+        # do validation here to follow the training process
         if val_loader:
             print("\n➡ Validation")
             model.eval()
@@ -390,14 +394,14 @@ def train(model, optimizer, train_loader, criterion, batch_size, epochs, schedul
                         'optimizer': optimizer.state_dict(),
                         'scheduler': scheduler.state_dict() if scheduler else None,
                     }
-                    save_checkpoint(ckpt, model_name + '_at_epoch' + str(epoch+1) +'.pth')
+                    save_checkpoint(ckpt, model_name + '_at_epoch' + str(epoch + 1) + '.pth')
                     # torch.save(model.state_dict(), os.path.join(model_path, model_name + f'_better_valacc_at_epoch_{epoch+1}.pth'))
                 if val_acc > curr_acc:
                     print("⚠ Detected decreasing validation accuracy!")
                 if losses_val.avg > val_loss:
                     print("⚠ Detected increasing validation loss!")
             val_loss = losses_val.avg
-                    
+
         # torch.save(model.state_dict(), os.path.join(model_path, model_name + f'_epoch_{epoch+1}.pth'))
         if scheduler is not None:
             scheduler.step()
@@ -406,7 +410,7 @@ def train(model, optimizer, train_loader, criterion, batch_size, epochs, schedul
     print(f"Epochs finished {epoch + 1} / {epochs}.")
     if val_loader:
         print(f"Best validation accuracy achieved at epoch {best_epoch} with {val_acc:.3f}.")
-    
+
 
 def predict(model, test_loader):
     print("\n➡ Prediction")
@@ -415,7 +419,7 @@ def predict(model, test_loader):
     # List of tuples which the model chose as most similar
     model.eval()
     with torch.no_grad():
-        for (ima, imb, imc) in tqdm(test_loader, leave=True, total= len(test_loader)):
+        for (ima, imb, imc) in tqdm(test_loader, leave=True, total=len(test_loader)):
             # Get embeddings from our model
             ima = ima.to(device)
             imb = imb.to(device)
@@ -445,8 +449,9 @@ def load_checkpoint(filename, model, optimizer, scheduler=None):
     print("Model at " + os.path.join(path, MODEL_PATH + MODEL_FILE_NAME) + ' loaded.')
     return ckpt['epoch'], model, optimizer, scheduler
 
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', action="store_true")
     parser.add_argument('--no_validation', action="store_true")
@@ -461,7 +466,7 @@ if __name__ == '__main__':
     parser.add_argument('--criterion', type=str, choices=['dist', 'cos_sim'], default='cos_sim')
     parser.add_argument('--step_lr', action="store_true")
     args = parser.parse_args()
-    
+
     VALIDATION = not args.no_validation
     if VALIDATION:
         assert 0 < args.validation_prop and args.validation_prop < 1, ValueError("Wrong validation proportion!")
@@ -477,7 +482,7 @@ if __name__ == '__main__':
     LR = args.lr
     CRITERION = args.criterion
     STEP_LR = args.step_lr
-    
+
     global path
     path = os.getcwd()
     if 'dataset' not in os.listdir():
@@ -489,24 +494,24 @@ if __name__ == '__main__':
     print("Image path: " + path_img)
     global model_path
     model_path = os.path.join(path, MODEL_PATH)
-    
+
     global device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using device {}.".format(device))
     torch.manual_seed(42)
     np.random.seed(42)
-    
+
     # preprocess on data
     prep = Preprocess(img_path=path_img, lbl_path=path_label, use_preprocessed_img=True)
     global images, train_triplets, validation_triplets, test_triplets
     images, train_triplets, validation_triplets, test_triplets = prep.prep_pipeline()
-    
+
     # Initialize the pre-trained network
     model_type = 'resnet'
     model_pretrained, input_size = initialize_model(model_type, FREEZE_PRETRAINED_FEATURES, use_pretrained=True)
     print("\n✅ Pre-trained Model Loaded")
     # print(model_pretrained)
-    
+
     model_pretrained = model_pretrained.to(device)
     params_to_update = model_pretrained.parameters()
     # print("Params to learn:")
@@ -541,53 +546,55 @@ if __name__ == '__main__':
     if TRAIN:
         train_set = FoodDataSet(train_triplets, transform=train_tf)
         train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
-        
+
     valid_loader = None
     if VALIDATION:
         validation_set = FoodDataSet(validation_triplets, transform=validation_tf)
         valid_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
-        
+
     if TEST:
         test_tf = validation_tf
         test_set = FoodDataSet(test_triplets, transform=test_tf)
-        test_loader = DataLoader(test_set, batch_size=BATCH_SIZE*2, shuffle=False, drop_last=False)
-    
+        test_loader = DataLoader(test_set, batch_size=BATCH_SIZE * 2, shuffle=False, drop_last=False)
+
     if FREEZE_PRETRAINED_FEATURES:
         optimizer = torch.optim.Adam(params_to_update, lr=LR)
     else:
         if model_type == 'densenet':
             optimizer = torch.optim.Adam([
-            {'params': model_pretrained.features.parameters(), 'lr': 2.5e-5},
-            {'params': model_pretrained.classifier.parameters(), 'lr': LR},
+                {'params': model_pretrained.features.parameters(), 'lr': 2.5e-5},
+                {'params': model_pretrained.classifier.parameters(), 'lr': LR},
             ], lr=LR)
         elif model_type == 'resnet':
             optimizer = torch.optim.Adam([
-                {'params': params_trained, 'lr': LR*0.25},
+                {'params': params_trained, 'lr': LR * 0.25},
                 {'params': params_fc, 'lr': LR}
-                ])
+            ])
     if CRITERION == 'dist':
         criterion = nn.TripletMarginLoss(margin=0.6)
         # reference: https://towardsdatascience.com/triplet-loss-advanced-intro-49a07b7d8905
     elif CRITERION == 'cos_sim':
         criterion = nn.TripletMarginWithDistanceLoss(
-            distance_function=lambda x, y: 1.0 - nn.functional.cosine_similarity(x, y), 
+            distance_function=lambda x, y: 1.0 - nn.functional.cosine_similarity(x, y),
             margin=0.25
         )
-    
+
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.4) if STEP_LR else None
-    
+
     starting_epoch = 0
     if LOAD_MODEL:
         try:
-            starting_epoch, model_pretrained, optimizer, scheduler = load_checkpoint(os.path.join(path, MODEL_PATH + MODEL_FILE_NAME), model_pretrained, optimizer, scheduler)
+            starting_epoch, model_pretrained, optimizer, scheduler = load_checkpoint(
+                os.path.join(path, MODEL_PATH + MODEL_FILE_NAME), model_pretrained, optimizer, scheduler)
             print("Model loaded!")
         except:
             ValueError("Wrong load configuration!")
-            
+
     if TRAIN:
-        train(model=model_pretrained, optimizer=optimizer, train_loader=train_loader, criterion=criterion, 
-            batch_size=BATCH_SIZE, epochs=EPOCHS, scheduler=scheduler, model_name='model', val_loader=valid_loader, starting_epoch=starting_epoch)
-    
+        train(model=model_pretrained, optimizer=optimizer, train_loader=train_loader, criterion=criterion,
+              batch_size=BATCH_SIZE, epochs=EPOCHS, scheduler=scheduler, model_name='model', val_loader=valid_loader,
+              starting_epoch=starting_epoch)
+
     if TEST:
         assert model_pretrained, ValueError("No model loaded")
         results = predict(model=model_pretrained, test_loader=test_loader)
@@ -596,5 +603,5 @@ if __name__ == '__main__':
                 s = ''.join(str(r))
                 f.write(s + '\n')
         print("Output completed as " + str(os.path.join(path, 'prediction.txt')))
-    
+
     print("---------- Execution finished! ----------")
