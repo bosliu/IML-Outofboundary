ea31b671c35815382a25929bc88d035256c1a8de
juhe9842 <juhe9842@colorado.edu>
2022-05-20 22:16:56 +0800

    pretrain pipeline

---
+++ task4/AE.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import random
+import matplotlib.pyplot as plt
+# Commented out IPython magic to ensure Python compatibility.
+# %cd /content/drive/MyDrive/task4
+os.getcwd()
+
+
+class Data:
+    def __init__(self, dpath):
+        self.pfeatures = pd.read_csv(os.path.join(
+            dpath, 'pretrain_features.csv')).to_numpy()[:, 2:].astype(float)
+        self.plabels = pd.read_csv(os.path.join(
+            dpath, 'pretrain_labels.csv')).to_numpy()[:, 2:].astype(float)
+        self.tfeatures = pd.read_csv(os.path.join(
+            dpath, 'train_features.csv')).to_numpy()[:, 2:].astype(float)
+        self.tlabels = pd.read_csv(os.path.join(
+            dpath, 'train_labels.csv')).to_numpy()[:, 2:].astype(float)
+        self.testfeatures = pd.read_csv(os.path.join(
+            dpath, 'test_features.csv')).to_numpy()[:, 2:].astype(float)
+
+
+path = os.getcwd()
+dpath = os.path.join(path, 'data')
+data = Data(dpath)
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        self.Encoded_dim = 128
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=1000, out_features=512),
+            nn.PReLU(),
+            nn.Linear(in_features=512, out_features=256),
+            nn.PReLU(),
+            nn.Linear(in_features=256, out_features=self.Encoded_dim),
+            # nn.PReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=self.Encoded_dim, out_features=256),
+            nn.PReLU(),
+            nn.Linear(in_features=256, out_features=512),
+            nn.PReLU(),
+            nn.Linear(in_features=512, out_features=1000),
+        )
+
+    def forward(self, x):
+        enc = self.encoder(x)
+        return self.decoder(enc)
+
+
+class MoleculeDataSet(Dataset):
+    def __init__(self, Data):
+        self.data = Data
+
+    def __getitem__(self, index):
+        a = self.data[index, :]
+        return a
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+def train(model, epochs, train_loader):
+    outputs = []
+    losses = []
+    loss = 0
+    for epoch in range(epochs):
+        for i, x in enumerate(train_loader):
+            reconstructed = model(x.float())
+            loss = loss_function(reconstructed, x.float())
+            print("Batch ", i, ": ", loss)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        losses.append(loss.detach().numpy())
+        outputs.append((epoch, x.float(), reconstructed))
+        print((epoch, x.float(), reconstructed))
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.plot(losses)
+    plt.show()
+
+    return outputs
+
+
+def worker_init_fn(worker_id):
+    torch_seed = torch.initial_seed()
+    random.seed(torch_seed + worker_id)
+    if torch_seed >= 2**30:
+        torch_seed = torch_seed % 2**30
+    np.random.seed(torch_seed + worker_id)
+
+
+BATCH_SIZE = 100
+model = AutoEncoder()
+loss_function = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr=1e-1,
+                             weight_decay=1e-8)
+
+train_set = MoleculeDataSet(data.pfeatures)
+train_loader = DataLoader(
+    train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+
+outputs = train(model, 100, train_loader)
--- task4/task4.py
+++ task4/task4.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
+import random
+
 # Commented out IPython magic to ensure Python compatibility.
 # %cd /content/drive/MyDrive/task4
 os.getcwd()
@@ -16,11 +18,11 @@ class Data:
         self.pfeatures = pd.read_csv(os.path.join(
             dpath, 'pretrain_features.csv')).to_numpy()[:, 2:].astype(float)
         self.plabels = pd.read_csv(os.path.join(
-            dpath, 'pretrain_labels.csv')).to_numpy()[:, 2:].astype(float)
+            dpath, 'pretrain_labels.csv')).to_numpy()[:, 1:].astype(float)
         self.tfeatures = pd.read_csv(os.path.join(
             dpath, 'train_features.csv')).to_numpy()[:, 2:].astype(float)
         self.tlabels = pd.read_csv(os.path.join(
-            dpath, 'train_labels.csv')).to_numpy()[:, 2:].astype(float)
+            dpath, 'train_labels.csv')).to_numpy()[:, 1:].astype(float)
         self.testfeatures = pd.read_csv(os.path.join(
             dpath, 'test_features.csv')).to_numpy()[:, 2:].astype(float)
 
@@ -51,49 +53,132 @@ class AutoEncoder(nn.Module):
         )
 
     def forward(self, x):
-        enc = self.encoder(x)
-        return self.decoder(enc)
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(in_features=128, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=32),
+            nn.ReLU(),
+            nn.Linear(in_features=32, out_features=1),
+        )
+
+    def forward(self, x):
+        outputs = self.linear_relu_stack(x)
+        return outputs
 
 
 class MoleculeDataSet(Dataset):
-    def __init__(self, Data):
-        self.data = Data
+    def __init__(self, Data, Data_reduced, mode):
+        self.mode = mode
+        if self.mode == 'AE':
+            self.inp = Data.pfeatures
+        elif self.mode == 'pretrain':
+            self.inp = Data_reduced
+            self.oup = Data.plabels
 
     def __getitem__(self, index):
-        a = self.data[index, :]
-        return a
+        if self.mode == 'AE':
+            inpt = self.inp[index, :]
+            return {'inp': inpt
+                    }
+
+        elif self.mode == 'pretrain':
+            inpt = self.inp[index, :]
+            oupt = self.oup[index]
+            return {'inp': inpt,
+                    'oup': oupt,
+                    }
 
     def __len__(self):
-        return self.data.shape[0]
+        return self.inp.shape[0]
 
 
-def train(model, epochs, train_loader):
+def worker_init_fn(worker_id):
+    torch_seed = torch.initial_seed()
+    random.seed(torch_seed + worker_id)
+    if torch_seed >= 2**30:
+        torch_seed = torch_seed % 2**30
+    np.random.seed(torch_seed + worker_id)
+
+
+def train(model, epochs, train_loader, optimizer, mode):
     outputs = []
     losses = []
     for epoch in range(epochs):
-        for i, x in enumerate(train_loader):
-            reconstructed = model(x.float())
-            loss = loss_function(reconstructed, x.float())
-            print("Batch ", i, ": ", loss)
+        if mode == "AE":
+            for i, x in enumerate(train_loader):
+                reconstructed = model(x["inp"].float())
+                loss = loss_function(reconstructed, x["inp"].float())
+                print("Batch ", i, ": ", loss)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            losses.append(loss)
-            outputs.append((epochs, x.float(), reconstructed))
+                losses.append(loss)
+            outputs.append((epoch, x["inp"].float(), reconstructed))
+            print(outputs)
+        elif mode == "pretrain":
+            for i, batch in enumerate(train_loader):
+                x_train, y_train = batch['inp'], batch['oup']
+                output = model(x_train.float())
+                loss = loss_function(output, y_train.float())
+                print("Batch ", i, ": ", loss)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                losses.append(loss)
+            outputs.append((epoch, y_train.float(), output))
+            print(outputs)
     return outputs
 
 
-BATCH_SIZE = 100
-model = AutoEncoder()
 loss_function = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(),
+BATCH_SIZE = 100
+
+# Training autoencoder
+model_AE = AutoEncoder()
+optimizer = torch.optim.Adam(model_AE.parameters(),
                              lr=1e-1,
                              weight_decay=1e-8)
 
-train_set = MoleculeDataSet(data.pfeatures)
-train_loader = DataLoader(
-    train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+ptrain_set = MoleculeDataSet(data,
+                             Data_reduced=[],
+                             mode="AE")
+ptrain_loader = DataLoader(ptrain_set, batch_size=BATCH_SIZE,
+                           shuffle=True,
+                           drop_last=False,
+                           )
 
-outputs = train(model, 20, train_loader)
+outputs = train(model_AE, 100, ptrain_loader, optimizer, mode="AE")
+
+
+# get the reduced features
+ptrain_tensor = torch.from_numpy(data.pfeatures).float()
+pfeatures_reduced = model_AE.encoder(ptrain_tensor).detach().numpy()
+
+# pre-train 50000 data with labels
+model_nn = NeuralNetwork()
+optimizer = torch.optim.Adam(model_nn.parameters(),
+                             lr=1e-3
+                             )
+
+ptrain_reduced_set = MoleculeDataSet(data,
+                                     Data_reduced=pfeatures_reduced,
+                                     mode="pretrain")
+ptrain_reduced_loader = DataLoader(ptrain_reduced_set, batch_size=BATCH_SIZE,
+                                   shuffle=True,
+                                   drop_last=False,
+                                   )
+
+outputs = train(model_nn, 100, ptrain_reduced_loader,
+                optimizer, mode="pretrain")
