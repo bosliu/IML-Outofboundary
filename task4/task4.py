8ae613be01c85cd1f47f2ed5250a53327fc3624a
juhe9842 <juhe9842@colorado.edu>
2022-05-21 21:34:01 +0800

    Finish compelete pipeline

--- task4/task4.py
+++ task4/task4.py
@@ -8,6 +8,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from tqdm import tqdm
 from dataclasses import dataclass
 import random
+import csv
 
 
 # Commented out IPython magic to ensure Python compatibility.
@@ -25,6 +26,8 @@ class Data:
             dpath, 'train_labels.csv')).to_numpy()[:, 1:].astype(float)
         self.testfeatures = pd.read_csv(os.path.join(
             dpath, 'test_features.csv')).to_numpy()[:, 2:].astype(float)
+        self.test_ID = pd.read_csv(os.path.join(
+            dpath, 'test_features.csv')).to_numpy()[:, 0]
 
 
 class AutoEncoder(nn.Module):
@@ -82,6 +85,9 @@ class MoleculeDataSet(Dataset):
         elif self.mode == 'pretrain':
             self.inp = Data_reduced
             self.oup = Data.plabels
+        elif self.mode == 'retrain':
+            self.inp = Data_reduced
+            self.oup = Data.tlabels
 
     def __getitem__(self, index):
         if self.mode == 'AE':
@@ -89,7 +95,7 @@ class MoleculeDataSet(Dataset):
             return {'inp': inpt
                     }
 
-        elif self.mode == 'pretrain':
+        elif self.mode == 'pretrain' or self.mode == 'retrain':
             inpt = self.inp[index, :]
             oupt = self.oup[index]
             return {'inp': inpt,
@@ -117,19 +123,19 @@ class AverageMeter:
 
     def __str__(self):
         return f'{self.name}: {self.val:.3e}, avg: {self.avg:.3e}'
-    
-
-def worker_init_fn(worker_id):
-    torch_seed = torch.initial_seed()
-    random.seed(torch_seed + worker_id)
-    if torch_seed >= 2**30:
-        torch_seed = torch_seed % 2**30
-    np.random.seed(torch_seed + worker_id)
 
 
-def train(model, epochs, train_loader, validation_loader, optimizer, scheduler, mode, to_save = False):
+# def worker_init_fn(worker_id):
+#     torch_seed = torch.initial_seed()
+#     random.seed(torch_seed + worker_id)
+#     if torch_seed >= 2**30:
+#         torch_seed = torch_seed % 2**30
+#     np.random.seed(torch_seed + worker_id)
+
+
+def train(model, epochs, train_loader, validation_loader, optimizer, scheduler, mode, to_save=False):
     outputs = []
-    pbar = tqdm(desc='Training', total= epochs * len(train_loader), leave=False)
+    pbar = tqdm(desc='Training', total=epochs * len(train_loader), leave=False)
     losses_val_hist = 1e3 if validation_loader else None
     val_loss_inc_epochs, val_loss_inc_epochs_max = 0, 5
     for epoch in range(epochs):
@@ -152,10 +158,11 @@ def train(model, epochs, train_loader, validation_loader, optimizer, scheduler, 
                 pbar.update()
             outputs.append((epoch, x_, reconstructed))
             # print(outputs)
-        elif mode == "pretrain":
+        elif mode == "train":
             for i, batch in enumerate(train_loader):
                 pbar.set_description_str(f'Epoch {epoch + 1}')
-                x_train, y_train = batch['inp'].to(device), batch['oup'].to(device)
+                x_train, y_train = batch['inp'].to(
+                    device), batch['oup'].to(device)
                 output = model(x_train.float())
                 loss = loss_function(output, y_train.float())
 
@@ -180,26 +187,28 @@ def train(model, epochs, train_loader, validation_loader, optimizer, scheduler, 
                     loss = loss_function(reconstructed, x_)
                     losses_val.update(loss.item(), x_.shape[0])
                 elif mode == "pretrain":
-                    x_train, y_train = batch['inp'].to(device), batch['oup'].to(device)
+                    x_train, y_train = batch['inp'].to(
+                        device), batch['oup'].to(device)
                     output = model(x_train.float())
                     loss = loss_function(output, y_train.float())
                     losses_val.update(loss.item(), x_train.shape[0])
             print("\nValidation average loss {:.3e}.".format(losses_val.avg))
             if losses_val.avg > losses_val_hist:
                 val_loss_inc_epochs += 1
-                print(f"Detected increasing validation loss! Stop training if continuing increasing for {val_loss_inc_epochs_max - val_loss_inc_epochs} epochs!")
+                print(
+                    f"Detected increasing validation loss! Stop training if continuing increasing for {val_loss_inc_epochs_max - val_loss_inc_epochs} epochs!")
             else:
                 val_loss_inc_epochs = 0
             if to_save:
                 ckpt = {
-                        'epoch': epoch,
-                        'model_state': model.state_dict(),
-                        'optimizer': optimizer.state_dict(),
-                        'scheduler': scheduler.state_dict() if scheduler else None,
-                    }
+                    'epoch': epoch,
+                    'model_state': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'scheduler': scheduler.state_dict() if scheduler else None,
+                }
                 save_checkpoint(ckpt, str(mode) + 'best.pth')
-            
-            losses_val_hist = losses_val.avg 
+
+            losses_val_hist = losses_val.avg
         if scheduler is not None:
             scheduler.step()
     return outputs
@@ -208,12 +217,13 @@ def train(model, epochs, train_loader, validation_loader, optimizer, scheduler, 
 def save_checkpoint(state, filename):
     torch.save(state, os.path.join(path, 'model' + filename))
     print("Model saved!")
-    
+
 
 BATCH_SIZE = 256
 VALIDATION_PROP = 0.2
 EPOCHS_AE = 200
 EPOCHS_PT = 100
+EPOCHS_RT = 100
 
 
 if __name__ == '__main__':
@@ -224,27 +234,37 @@ if __name__ == '__main__':
 
     loss_function = nn.MSELoss()
 
+    #############################
     # Training autoencoder
+    #############################
     model_AE = AutoEncoder().to(device)
     optimizer = torch.optim.Adam(model_AE.parameters(),
-                                lr=1e-2,
-                                weight_decay=1e-8)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.5)
+                                 lr=1e-2,
+                                 weight_decay=1e-8)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=15, gamma=0.5)
 
     ptrain_set = MoleculeDataSet(data,
-                                Data_reduced=[],
-                                mode="AE")
+                                 Data_reduced=[],
+                                 mode="AE")
     train_len = int((1 - VALIDATION_PROP) * len(ptrain_set))
-    ptrain_t_set, ptrain_v_set = random_split(ptrain_set, [train_len, len(ptrain_set) - train_len])
+    ptrain_t_set, ptrain_v_set = random_split(
+        ptrain_set, [train_len, len(ptrain_set) - train_len])
     ptrain_loader = DataLoader(ptrain_set, batch_size=BATCH_SIZE,
-                            shuffle=False,
-                            drop_last=False,
-                            )  # for all pretrain data, prediction
-    ptrain_t_loader = DataLoader(ptrain_t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
-    ptrain_v_loader = DataLoader(ptrain_v_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+                               shuffle=False,
+                               drop_last=False,
+                               )  # for all pretrain data, prediction
+    ptrain_t_loader = DataLoader(
+        ptrain_t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+    ptrain_v_loader = DataLoader(
+        ptrain_v_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
 
-    outputs = train(model_AE, EPOCHS_AE, ptrain_t_loader, ptrain_v_loader, optimizer, scheduler, mode="AE")
+    outputs = train(model_AE, EPOCHS_AE, ptrain_t_loader,
+                    ptrain_v_loader, optimizer, scheduler, mode="AE")
 
+    #############################
+    # Pretraining neural-network
+    #############################
 
     # get the reduced features
     model_AE.eval()
@@ -254,19 +274,75 @@ if __name__ == '__main__':
     # pre-train 50000 data with labels
     model_nn = NeuralNetwork().to(device)
     optimizer = torch.optim.Adam(model_nn.parameters(),
-                                lr=1e-3
-                                )
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.25)
+                                 lr=1e-3
+                                 )
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=10, gamma=0.25)
 
     ptrain_reduced_set = MoleculeDataSet(data,
-                                        Data_reduced=pfeatures_reduced,
-                                        mode="pretrain")
-    ptrain_red_t_set, ptrain_red_v_set = random_split(ptrain_reduced_set, [train_len, len(ptrain_set) - train_len])
+                                         Data_reduced=pfeatures_reduced,
+                                         mode="pretrain")
+    ptrain_red_t_set, ptrain_red_v_set = random_split(
+        ptrain_reduced_set, [train_len, len(ptrain_set) - train_len])
     ptrain_reduced_loader = DataLoader(ptrain_reduced_set, batch_size=BATCH_SIZE,
-                                    shuffle=True,
-                                    drop_last=False,
-                                    )
-    ptrain_red_t_loader = DataLoader(ptrain_red_t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
-    ptrain_red_v_loader = DataLoader(ptrain_red_v_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+                                       shuffle=True,
+                                       drop_last=False,
+                                       )
+    ptrain_red_t_loader = DataLoader(
+        ptrain_red_t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+    ptrain_red_v_loader = DataLoader(
+        ptrain_red_v_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
 
-    outputs = train(model_nn, EPOCHS_PT, ptrain_red_t_loader, ptrain_red_v_loader, optimizer, scheduler, mode="pretrain")
+    outputs = train(model_nn, EPOCHS_PT, ptrain_red_t_loader,
+                    ptrain_red_v_loader, optimizer, scheduler, mode="train")
+
+    #############################
+    # Retraining neural-network
+    #############################
+
+    # get the reduced features
+    model_AE.eval()
+    train_tensor = torch.from_numpy(data.tfeatures).float().to(device)
+    tfeatures_reduced = model_AE.encoder(train_tensor).detach().cpu().numpy()
+
+    # retrain nn model using 100 data with target labels
+    # model_nn = NeuralNetwork().to(device)
+    optimizer = torch.optim.Adam(model_nn.parameters(),
+                                 lr=1e-4
+                                 )
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=10, gamma=0.25)
+
+    train_reduced_set = MoleculeDataSet(data,
+                                        Data_reduced=tfeatures_reduced,
+                                        mode="retrain")
+    train_red_t_set, train_red_v_set = random_split(
+        train_reduced_set, [train_len, len(train_reduced_set) - train_len])
+    train_reduced_loader = DataLoader(train_reduced_set, batch_size=BATCH_SIZE,
+                                      shuffle=True,
+                                      drop_last=False,
+                                      )
+    train_red_t_loader = DataLoader(
+        train_red_t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+    train_red_v_loader = DataLoader(
+        ptrain_red_v_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+
+    outputs = train(model_nn, EPOCHS_RT, train_red_t_loader,
+                    train_red_v_loader, optimizer, scheduler, mode="train")
+
+    #############################
+    # Test
+    #############################
+    # get the reduced features
+    model_AE.eval()
+    test_tensor = torch.from_numpy(data.testfeatures).float().to(device)
+    testfeatures_reduced = model_AE.encoder(test_tensor)
+    prediction = model_nn(testfeatures_reduced).detach().cpu().numpy()
+    IDs = data.test_ID
+    with open('prediction.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        for i in range(len(IDs)):
+            if i == 0:
+                writer.writerow(['Id', 'y'])
+            else:
+                writer.writerow([IDs[i], prediction[i][0]])
